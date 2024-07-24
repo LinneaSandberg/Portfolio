7e326f48569f86ca6c0dcb39ea3b8269df3b3bbb
Linnea Sandberg <llinnea.sandberg@gmail.com>
2024-07-24 08:10:15 +0200

    installed dependencies and setup of both queryClient and reactRouter.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import "./assets/scss/App.scss";
+import { Route, Routes } from "react-router-dom";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
 
   return (
-    <>
-    </>
+    <div id="App">
+
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+
+      <ReactQueryDevtools />
+    </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      gcTime: 1000 * 60 * 60, // 1 hour
+    },
+  },
+
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
