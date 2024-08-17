f5d7078dee735ac9b0ba274540e79392281df2bd
Linnea Sandberg <llinnea.sandberg@gmail.com>
2024-08-17 07:12:50 +0200

    prepared a react hook Form to handle the contactForm

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,8 +1,32 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
 import githubLogo from '../../assets/images/github.png';
 import linkedinLogo from '../../assets/images/linkedin.svg';
 
+interface Inputs {
+    Firstname: string;
+    Lastname: string;
+    Email: string;
+    Phone: string;
+    Subject: string;
+    Message: string;
+}
 
 const ContactSection = () => {
+
+    const {
+        register,
+        handleSubmit,
+        // watch,
+        // formState: { errors },
+    } = useForm<Inputs>()
+
+    const onSubmit: SubmitHandler<Inputs> = async ({ Firstname, Lastname, Email, Phone, Subject, Message
+    }) => {
+        console.log(Firstname, Lastname, Email, Phone, Subject, Message);
+    }
+
+
+
 
     return (
         <section className="section-wrapper contact-section" id="section-contact">
@@ -16,32 +40,68 @@ const ContactSection = () => {
                     <img src={linkedinLogo} alt="LinkedIn" />
                 </figure>
             </div>
-            <form className="contact-form">
+            <form className="contact-form" onSubmit={handleSubmit(onSubmit)}>
                 <div className="input-wrapper">
                     <div className="inputs input-size">
-                        <input type="text" id="Firstname" name="Firstname" placeholder="FIRST NAME *" className="input-field" required />
+                        <input
+                            type="text"
+                            id="Firstname"
+                            placeholder="FIRST NAME *"
+                            className="input-field"
+                            {...register("Firstname", { required: true })}
+                        />
                     </div>
                     <div className="inputs input-size">
-                        <input type="text" id="Lastname" name="Lastname" placeholder="LAST NAME *" className="input-field" required />
+                        <input
+                            type="text"
+                            id="Lastname"
+                            placeholder="LAST NAME *"
+                            className="input-field"
+                            {...register("Lastname", { required: true })}
+                        />
                     </div>
                 </div>
 
                 <div className="input-wrapper">
                     <div className="inputs input-size">
-                        <input type="text" id="Email" name="Email" placeholder="EMAIL *" className="input-field" required />
+                        <input
+                            type="email"
+                            id="email"
+                            placeholder="EMAIL *"
+                            className="input-field"
+                            {...register("Email", { required: true })}
+                        />
                     </div>
 
                     <div className="inputs input-size">
-                        <input type="text" id="Phone" name="Phone" placeholder="PHONE" className="input-field" required />
+                        <input
+                            type="text"
+                            id="Phone"
+                            placeholder="PHONE"
+                            className="input-field"
+                            {...register("Phone")}
+
+                        />
                     </div>
                 </div>
 
                 <div className="input-wrapper inputs">
-                    <input type="text" id="Subject" name="Subject" placeholder="SUBJECT *" className="input-field" required />
+                    <input
+                        type="text"
+                        id="Subject"
+                        placeholder="SUBJECT *"
+                        className="input-field"
+                        {...register("Subject", { required: true })}
+                    />
                 </div>
 
                 <div className="input-wrapper inputs">
-                    <textarea id="Message" name="Message" placeholder="MESSAGE *" className="input-field message" required></textarea>
+                    <textarea
+                        id="Message"
+                        placeholder="MESSAGE *"
+                        className="input-field message"
+                        {...register("Message", { required: true })}
+                    ></textarea>
                 </div>
 
                 <div className="input-wrapper">
