5655d7257f7fff30cdb86f492dc02f124bb07f16
Linnea Sandberg <llinnea.sandberg@gmail.com>
2024-09-11 17:37:55 +0200

    changed the skills-section from a sidescroll to instead be a gallary containing the same content

---
+++ src/components/CardCaru.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+interface CardProps {
+    text: string,
+    title: string,
+    icon: string,
+    btn1Text: string,
+    btn1Link: string,
+    sendInteraction?: (label: string) => void;
+}
+
+const Card: React.FC<CardProps> = ({ text, icon, btn1Text, btn1Link, sendInteraction }) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const toggleTextExpansion = () => {
+        setIsExpanded(!isExpanded);
+    };
+
+    const openUrl = (url: string, label: string) => {
+        window.open(url, '_blank');
+        if (sendInteraction && label) {
+            sendInteraction(label);
+        }
+    }
+
+    const displayedText = isExpanded ? text : (text.length > 40 ? text.substring(0, 40) + '...' : text);
+
+    return (
+        <li className="carousel-card">
+            <img src={icon} className='carousel-icon' alt="Icon" />
+            <div onClick={toggleTextExpansion} className="carousel-card-text">
+                <p>{displayedText}</p>
+            </div>
+            <a className="carousel-card-outline" href='#'
+                onClick={() => openUrl(btn1Link, btn1Text)}>
+                <span>&#8250; {btn1Text}</span>
+            </a>
+        </li>
+    )
+}
+
+export default Card;
--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -1,12 +1,58 @@
 import reactLogo from '../../assets/images/react-tur.svg';
 import javascriptLogo from '../../assets/images/javascript.svg';
+import React from 'react';
+import Carousel from '../Carousel';
 
-const SkillsSection = () => {
+const SkillsSection: React.FC = () => {
+
+    const items = [
+        {
+            icon: reactLogo,
+            title: "React",
+            text: "During diffrent projects I have worked with React. I have experience with React hooks, React Router, Context API, Redux, and Redux Toolkit.",
+            btn1Text: "The Movie DB",
+            btn1Link: "https://thebestmoviepage.netlify.app/ ",
+            // btn2Text: "Sulten",
+            // btn2Link: "https://sulten.netlify.app/  ",
+        },
+        {
+            icon: javascriptLogo,
+            title: "JavaScript",
+            text: "I have experience with JavaScript ES6, and I have worked with JavaScript libraries such as Axios, Lodash, and Moment.js.",
+            btn1Text: "Flags Quiz",
+            btn1Link: "https://quizwithflags.netlify.app/",
+        },
+        {
+            icon: javascriptLogo,
+            title: "TypeScript",
+            text: "During almost my hole eduction we been using TypeScript when lerning both JavaScript and React. From day one using TypeScript I felt that it made more sence to me than JavaScript. It gives me a better understanding of the code and makes it easier to read and understand.",
+            btn1Text: "Sulten",
+            btn1Link: "https://sulten.netlify.app/",
+        },
+        {
+            icon: javascriptLogo,
+            title: "Vue",
+            text: "Under my internship I worked with Vue.js. I have experience with Vue Router, Vuex, and Vue CLI.",
+            btn1Text: "",
+            btn1Link: "",
+        }
+    ];
+
+    const handleInteraction = (label: string) => {
+        console.log(`Interaction with: ${label}`);
+    };
 
     return (
         <section className="section-wrapper skills-wrapper" id="section-skills">
             <h2 className='skills-title'>Skills</h2>
-            <div className='skill-box'>
+            <Carousel items={items} sendInteraction={handleInteraction} />
+        </section >
+    )
+}
+
+export default SkillsSection;
+
+{/* <div className='skill-box'>
                 <h3>React</h3>
                 <img src={reactLogo} alt="React logo" width={200} />
                 <p className='skill-text'>
@@ -26,9 +72,4 @@ const SkillsSection = () => {
                 <p className='skill-text'>
                     During almost my hole eduction we been using TypeScript when lerning both JavaScript and React. From day one using TypeScript I felt that it made more sence to me than JavaScript. It gives me a better understanding of the code and makes it easier to read and understand.
                 </p>
-            </div>
-        </section >
-    )
-}
-
-export default SkillsSection;
+            </div> */}
