774dbe3b5d6cd034493684e845877d78d2c36b93
Linnea Sandberg <llinnea.sandberg@gmail.com>
2024-09-12 13:40:03 +0200

    changed order of diffrent sections

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -14,16 +14,16 @@ const Navigation = () => {
                     <a href="#section-about" data-title="About" className="pink"></a>
                 </li>
                 <li>
+                    <span className="text" title="Contact">Contact</span>
+                    <a href="#section-contact" data-title="Contact" className="yellow"></a>
+                </li>
+                <li>
                     <span className="text" title="Skills">Skills</span>
-                    <a href="#section-contact" data-title="Skills" className="yellow"></a>
+                    <a href="#section-skills" data-title="Skills" className="purple"></a>
                 </li>
                 <li>
                     <span className="text" title="Projects">Projects</span>
-                    <a href="#section-skills" data-title="Projects" className="purple"></a>
-                </li>
-                <li>
-                    <span className="text" title="Contact">Contact</span>
-                    <a href="#section-projects" data-title="Contact" className="red"></a>
+                    <a href="#section-projects" data-title="Projects" className="red"></a>
                 </li>
             </ul>
         </div>
