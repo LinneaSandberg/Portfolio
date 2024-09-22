1a78d3bc77b0f8d27fda04041c21446eacf543c2
Linnea Sandberg <llinnea.sandberg@gmail.com>
2024-09-22 09:09:04 +0200

    Added a img for Vue and removed unused images.

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -1,9 +1,9 @@
 import reactLogo from '../../assets/images/react.svg';
-import javascriptLogo from '../../assets/images/javascript.svg';
 import React from 'react';
 import Carousel from '../Carousel';
 import vue from "../../assets/images/vue.svg"
-import placeHolder from '../../assets/images/placeholder.webp';
+import typescriptLogo from '../../assets/images/typescript.svg';
+import jsLogo from '../../assets/images/js.svg';
 
 const SkillsSection: React.FC = () => {
 
@@ -16,14 +16,14 @@ const SkillsSection: React.FC = () => {
             btn1Link: "https://thebestmoviepage.netlify.app/ ",
         },
         {
-            icon: javascriptLogo,
+            icon: jsLogo,
             title: "JavaScript",
             text: "During my education, we gained a comprehensive understanding of programming concepts in general and developed skills specifically in JavaScript/EcmaScript and related areas. This enabled us to create interactive, user-centered websites individually and collaboratively. The course aimed to ensure that we understood general programming concepts and had a solid foundational knowledge of JavaScript, DOM manipulation, and event-driven programming, providing us with a holistic understanding of the components that make up a website and the ability to develop functional, interactive web applications as front-end developers.",
             btn1Text: "Flags Quiz",
             btn1Link: "https://quizwithflags.netlify.app/",
         },
         {
-            icon: placeHolder,
+            icon: typescriptLogo,
             title: "TypeScript",
             text: "During almost my whole education, we have been using TypeScript when learning both JavaScript and React. From day one of using TypeScript, I felt that it made more sense to me than JavaScript. It gives me a better understanding of the code and makes it easier to read and understand.",
             btn1Text: "Sulten",
